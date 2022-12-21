b298e61dfad43c53d76bb1ae3bf8406002856aa4
aamer Essa <aamer.es12@gmail.com>
2022-12-21 10:30:53 +0300

    V1.0

---
+++ SADO/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  SADO
+//
+//  Created by Aamer Essa on 21/12/2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
