0ef0c9db570847a040624d7df76e9bdf5946a4ff
aamer Essa <aamer.es12@gmail.com>
2022-12-21 10:02:42 +0300

    Initial Commit

---
+++ SADO/SADOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SADOApp.swift
+//  SADO
+//
+//  Created by Aamer Essa on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SADOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
